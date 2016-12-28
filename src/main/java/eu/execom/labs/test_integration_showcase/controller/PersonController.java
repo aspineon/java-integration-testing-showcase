1c84d14016e1a051b8a1e7b5b9d814bb8908df21
Srdjan Simidzija <ssimidzija@execom.eu>
2016-12-28 15:18:49 +0100

    Implemented REST methods in PersonController and added jdbc.properties file.

--- src/main/java/eu/execom/labs/test_integration_showcase/controller/PersonController.java
+++ src/main/java/eu/execom/labs/test_integration_showcase/controller/PersonController.java
@@ -1,35 +1,32 @@
 package eu.execom.labs.test_integration_showcase.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import eu.execom.labs.test_integration_showcase.dto.PersonDto;
-import eu.execom.labs.test_integration_showcase.entity.Person;
+import eu.execom.labs.test_integration_showcase.service.PersonService;
 
 @RestController
 public class PersonController {
 
+    @Autowired
+    private PersonService personService;
+
     @RequestMapping("/add")
     public PersonDto addPerson(PersonDto person) {
-        // TODO implement
-        return new PersonDto(new Person());
-
+        return personService.addPerson(person);
     }
 
     @RequestMapping("/persons")
-    public List<Person> findAll() {
-        // TODO implement
-        return new ArrayList<Person>();
-
+    public List<PersonDto> findAll() {
+        return personService.findAllPersons();
     }
 
     @RequestMapping("/getPerson")
-    public PersonDto getPersonBySsn(PersonDto person) {
-        // TODO implement
-        return new PersonDto(new Person());
-
+    public PersonDto getPersonBySsn(String ssn) {
+        return personService.getPersonBySsn(ssn);
     }
 }
