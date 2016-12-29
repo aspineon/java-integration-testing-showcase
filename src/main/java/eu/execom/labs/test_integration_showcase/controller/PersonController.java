7c9a2017d80ebb042899f05cc29d59353dc7eaa4
Srdjan Simidzija <ssimidzija@execom.eu>
2016-12-29 13:50:53 +0100

    Changed PersonController to have more REST like annotations, added validation on PersonDto class.

--- src/main/java/eu/execom/labs/test_integration_showcase/controller/PersonController.java
+++ src/main/java/eu/execom/labs/test_integration_showcase/controller/PersonController.java
@@ -2,7 +2,13 @@ package eu.execom.labs.test_integration_showcase.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,23 +16,24 @@ import eu.execom.labs.test_integration_showcase.dto.PersonDto;
 import eu.execom.labs.test_integration_showcase.service.PersonService;
 
 @RestController
+@RequestMapping("/persons")
 public class PersonController {
 
     @Autowired
     private PersonService personService;
 
-    @RequestMapping("/add")
-    public PersonDto addPerson(PersonDto person) {
+    @PostMapping
+    public PersonDto addPerson(@Valid @RequestBody PersonDto person) {
         return personService.addPerson(person);
     }
 
-    @RequestMapping("/persons")
+    @GetMapping
     public List<PersonDto> findAll() {
         return personService.findAllPersons();
     }
 
-    @RequestMapping("/getPerson")
-    public PersonDto getPersonBySsn(String ssn) {
+    @GetMapping("/{ssn}")
+    public PersonDto getPersonBySsn(@PathVariable String ssn) {
         return personService.getPersonBySsn(ssn);
     }
 }
--- src/main/java/eu/execom/labs/test_integration_showcase/dto/PersonDto.java
+++ src/main/java/eu/execom/labs/test_integration_showcase/dto/PersonDto.java
@@ -1,5 +1,10 @@
 package eu.execom.labs.test_integration_showcase.dto;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Email;
+
 import eu.execom.labs.test_integration_showcase.entity.Person;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,12 +15,19 @@ public class PersonDto {
 
     private long personId;
 
+    @NotNull
+    @Pattern(regexp = ("[0-3]{3}-[0-3]{3}-[0-3]{3}"))
     private String ssn;
 
+    @Email
     private String email;
 
+    @NotNull
+    @Pattern(regexp = "^\\w+( \\w*)*$")
     private String firstName;
 
+    @NotNull
+    @Pattern(regexp = "^\\w+( \\w*)*$")
     private String lastName;
 
     public PersonDto(Person person) {
